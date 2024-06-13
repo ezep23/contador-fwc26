a8995e2d2f270664abf0849ef796f373cb48126c
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-06-12 21:17:55 -0300

    agregando contador

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from datetime import datetime, timedelta
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return '¡Hola, mundo! Esta es una aplicación Flask básica.'
+def contador_regresivo():
+    fecha_objetivo = datetime(2026, 7, 11, 14, 30, 0)  # Establece la fecha y hora objetivo
+    while datetime.now() < fecha_objetivo:
+        tiempo_restante = fecha_objetivo - datetime.now()
+        tiempo_restante_segundos = tiempo_restante.total_seconds()
+        return render_template('contador', tiempo_restante=tiempo_restante_segundos)
+        time.sleep(1)  # Espera 1 segundo antes de volver a verificar
+    return redirect(url_for('tcompletado'))
+
+@app.route('/tcompletado')
+def tiempo_completado():
+    return "¡Tiempo completado!"
 
 if __name__ == '__main__':
     app.run(debug=True)
