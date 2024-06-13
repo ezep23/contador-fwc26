e25530d8b608593b5e63b927cb478843df286701
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-06-12 23:37:29 -0300

    añadiendo template principal

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, make_response, redirect, url_for
 from datetime import datetime, timedelta
 import time
 
 app = Flask(__name__)
 
-@app.route('/')
-def contador_regresivo():
+@app.route('/index')
+def index():
     fecha_objetivo = datetime(2026, 7, 11, 14, 30, 0)  # Establece la fecha y hora objetivo
     while datetime.now() < fecha_objetivo:
         tiempo_restante = fecha_objetivo - datetime.now()
         tiempo_restante_segundos = tiempo_restante.total_seconds()
-        return render_template('contador', tiempo_restante=tiempo_restante_segundos)
+        return render_template("index.html", tiempo_restante=tiempo_restante_segundos)
         time.sleep(1)  # Espera 1 segundo antes de volver a verificar
-    return redirect(url_for('tcompletado'))
+    return redirect(url_for('/tcompletado'))
 
 @app.route('/tcompletado')
 def tiempo_completado():
