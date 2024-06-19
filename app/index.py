9031e266ff96fd7724c6ebc77d78e35d6e1a07f4
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-06-19 17:50:55 -0300

    agregando estilos

--- app/index.py
+++ app/index.py
@@ -1,7 +1,7 @@
 # Importamos el paquete predeterminado de tiempo de python 
 from datetime import datetime, time, timedelta
 # Importamos el framework de Flask
-from flask import Flask, render_template, make_response, redirect, url_for
+from flask import Flask, render_template, make_response, redirect, url_for, jsonify
 
 app = Flask(__name__)
 
@@ -15,7 +15,7 @@ def not_found_endpoint(error):
     return render_template('error.html', error=error)
 
 # Endpoint index con el contador regresivo
-@app.route('/index')
+@app.route('/')
 def index():
     fecha_objetivo = datetime(2026, 7, 11, 14, 30, 0)  # Establece la fecha y hora objetivo
     if verificar_fecha(fecha_objetivo):
@@ -31,14 +31,14 @@ def index():
         minutes = (tiempo_restante.seconds // 60) % 60
         seconds = tiempo_restante.seconds % 60
         
-        return render_template("index.html", years=years, months=months, days=days,hours=hours, minutes=minutes, seconds=seconds)
+        return render_template('index.html', years=years, months=months, days=days,hours=hours, minutes=minutes, seconds=seconds)
 
 #Endpoint para cuando se complete el tiempo
 @app.route('/completado')
 def tiempo_completado():
     fecha_objetivo = datetime(2026, 7, 11, 14, 30, 0)  # Establece la fecha y hora objetivo
     if verificar_fecha(fecha_objetivo):
-        return render_template("completado.html")
+        return render_template('completado.html')
     else:
         return redirect(url_for('index'))
 
