8391e9859e17ec00dd3f509ef76661b2f60ad462
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-06-22 22:12:25 -0300

    contador funcionando correctamente

--- app/index.py
+++ app/index.py
@@ -17,21 +17,7 @@ def not_found_endpoint(error):
 # Endpoint index con el contador regresivo
 @app.route('/')
 def index():
-    fecha_objetivo = datetime(2026, 7, 11, 14, 30, 0)  # Establece la fecha y hora objetivo
-    if verificar_fecha(fecha_objetivo):
-        return redirect(url_for('tiempo_completado'))
-    else:
-        tiempo_restante = fecha_objetivo - datetime.now()
-
-        # Calcula años, meses, días, horas, minutos y segundos restantes
-        years = tiempo_restante.days // 365
-        months = tiempo_restante.days % 365 // 30
-        days = tiempo_restante.days % 365 % 30
-        hours = tiempo_restante.seconds // 3600
-        minutes = (tiempo_restante.seconds // 60) % 60
-        seconds = tiempo_restante.seconds % 60
-        
-        return render_template('index.html', years=years, months=months, days=days,hours=hours, minutes=minutes, seconds=seconds)
+    return render_template('index.html')
 
 #Endpoint para cuando se complete el tiempo
 @app.route('/completado')
